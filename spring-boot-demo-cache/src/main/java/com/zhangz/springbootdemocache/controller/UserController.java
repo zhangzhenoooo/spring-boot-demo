1205fe6602ca0706e603f3c2ee741a23a2a07132
zhangz <1804919062@qq.com>
2023-10-23 13:57:23 +0800

    feature: 1.springboot cache j2cache 实现二级缓存

--- spring-boot-demo-cache/src/main/java/com/zhangz/springbootdemocache/controller/UserController.java
+++ spring-boot-demo-cache/src/main/java/com/zhangz/springbootdemocache/controller/UserController.java
@@ -3,6 +3,7 @@ package com.zhangz.springbootdemocache.controller;
 import com.alibaba.fastjson.JSON;
 import com.zhangz.springbootdemocache.entity.User;
 import com.zhangz.springbootdemocache.service.UserService;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +19,8 @@ public class UserController {
 
     @GetMapping("/getUser")
     @ResponseBody
-    public String getUser() {
-        User u = userService.getOneDemoUser("123456");
+    public String getUser(@Param("userId") String userId) {
+        User u = userService.getOneDemoUser(userId);
         return JSON.toJSONString(u);
     }
 }
--- spring-boot-demo-cache/src/main/java/com/zhangz/springbootdemocache/service/impl/UserServiceImpl.java
+++ spring-boot-demo-cache/src/main/java/com/zhangz/springbootdemocache/service/impl/UserServiceImpl.java
@@ -12,14 +12,14 @@ public class UserServiceImpl implements UserService {
     /**
      * value 缓存名称，可指定多个缓存，返回其中命中的第一个；
      * key 缓存的key ，显示指定
-     * condition 条件，只缓存满足条件的
+     * condition 条件，只缓存满足条件的 condition = "#userId.equals('123456')"
      * @param userId
      * @return
      */
     @Override
-    @Cacheable(value = {"user"}, key = "'user_' + #userId", condition = "#userId.equals('123456')")
+    @Cacheable(value = {"user"}, key = "'user_' + #userId" )
     public User getOneDemoUser(String userId) {
-        User user = User.builder().userId("123456").userName("testname").build();
+        User user = User.builder().userId(userId).userName("testname").build();
         return user;
     }
 }
