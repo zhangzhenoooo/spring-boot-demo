9742f6c9636c3b98e5e1616d083b9601be3f5e4e
zhangz <1804919062@qq.com>
2023-11-01 17:04:15 +0800

    feature: 1.nacos 集成dubbo

---
+++ spring-boot-demo-confManager/src/main/java/com/zhangz/springbootdemoconfmanager/service/TestService.java
@@ -0,0 +1,9 @@
+package com.zhangz.springbootdemoconfmanager.service;
+
+public interface TestService {
+
+    String getShardvalue();
+
+    Object getSystemTestProperties();
+
+}
--- spring-boot-demo-confManager/src/main/java/com/zhangz/springbootdemoconfmanager/service/impl/TestServiceImpl.java
+++ spring-boot-demo-confManager/src/main/java/com/zhangz/springbootdemoconfmanager/service/impl/TestServiceImpl.java
@@ -1,18 +1,16 @@
-package com.zhangz.springbootdemoconfmanager.controller;
+package com.zhangz.springbootdemoconfmanager.service.impl;
 
 import com.alibaba.fastjson.JSON;
 import com.zhangz.springbootdemoconfmanager.config.SystemTestProperties;
+import com.zhangz.springbootdemoconfmanager.service.TestService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/test")
-@RefreshScope
-public class TestController {
+@Slf4j
+@DubboService
+public class TestServiceImpl implements TestService {
 
     @Value("${system.test.shardvalue}")
     private String shardvalue;
@@ -20,14 +18,13 @@ public class TestController {
     @Autowired
     private SystemTestProperties systemTestProperties;
 
-    @PostMapping("getShardValue")
+    @Override
     public String getShardvalue() {
         return shardvalue;
     }
 
-    @PostMapping("getSystemTestProperties")
+    @Override
     public Object getSystemTestProperties() {
         return JSON.toJSON(systemTestProperties);
     }
-
 }
