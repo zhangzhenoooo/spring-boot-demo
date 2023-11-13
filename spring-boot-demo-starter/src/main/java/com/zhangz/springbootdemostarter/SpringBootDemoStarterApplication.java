7c5a28699b9fe548516871a515a7bd01ef9a1c9f
zhangz <1804919062@qq.com>
2023-11-13 17:19:53 +0800

    feature: 1.删除module中的启动类

--- spring-boot-demo-starter/src/main/java/com/zhangz/springbootdemostarter/SpringBootDemoStarterApplication.java
+++ spring-boot-demo-starter/src/main/java/com/zhangz/springbootdemostarter/SpringBootDemoStarterApplication.java
@@ -1,7 +1,7 @@
 package com.zhangz.springbootdemostarter;
 
-import org.apache.dubbo.config.spring.context.annotation.EnableDubboConfig;
-import org.mybatis.spring.annotation.MapperScan;
+ import com.alibaba.dubbo.config.spring.context.annotation.EnableDubboConfig;
+ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
